1db7e89231c9ca5478221e91471c0751cdf04e1f
Ehiz-py <you@example.com>
2025-03-26 21:53:18 +0100

    update

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import Search from "./components/Search";
+import { useEffect, useState } from "react";
+
+const API_BASE_URL = "https://api.themoviedb.org/3/discover/movie";
 
 const App = () => {
+	const [searchTerm, setSearchTerm] = useState("");
+	useEffect(() => {}, []);
+
 	return (
-		<div>
-			<h1 className="text-3xl font-bold underline text-red-500">
-				Hello world!
-			</h1>
-		</div>
+		<main>
+			<div className="pattern" />
+			<div className="wrapper">
+				<header>
+					<img src="./hero.png" alt="Hero Banner" />
+					<h1>
+						Find <span className="text-gradient">Movies</span> You'll Enjoy
+						Without The Hassle
+					</h1>
+				</header>
+				<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+				<h1 className="text-white">{searchTerm}</h1>
+			</div>
+		</main>
 	);
 };
 
